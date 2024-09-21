70f259b29883b600394af7fed6cc98e7b6e7908a
BouajilaHamza <hamza.bouajila@enetcom.u-sfax.tn>
2024-09-21 11:03:26 +0100

    add requirements.txt and update main

--- app/app/main.py
+++ app/app/main.py
@@ -1,30 +1,35 @@
-import litserve as ls
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+from litserve import LitAPI, LitServer
 
-class SimpleLitAPI(ls.LitAPI):
+class BERTLitAPI(LitAPI):
     def setup(self, device):
-        self.model1 = lambda x: x**2
-        self.model2 = lambda x: x**3
+        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+        self.model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased")
+        self.model.to(device)
+        self.model.eval()
 
     def decode_request(self, request):
-        return request["input"]
+        text = request["text"]
+        inputs = self.tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=512)
+        return inputs
 
-    def predict(self, x):
-        squared = self.model1(x)
-        cubed = self.model2(x)
-        output = squared + cubed
-        yield {"output": output}
+    def predict(self, inputs):
+        with torch.no_grad():
+            inputs = {k: v.to(self.model.device) for k, v in inputs.items()}
+            outputs = self.model(**inputs)
+        return outputs.logits
 
-    def encode_response(self, output):
-        yield {"output": output}
-    
-    def unbatch(self,response):
-        yield response
+    def encode_response(self, logits):
+        probabilities = torch.nn.functional.softmax(logits, dim=-1)
+        response = {
+            "positive": probabilities[:, 1].item(),
+            "negative": probabilities[:, 0].item()
+        }
+        return response
 
 if __name__ == "__main__":
-    api = SimpleLitAPI()
-    server = ls.LitServer(api,accelerator="auto")
+    api = BERTLitAPI()
+    server = LitServer(api, accelerator='auto', workers_per_device=2)
     server.run(port=8000)
-
-
-
   
