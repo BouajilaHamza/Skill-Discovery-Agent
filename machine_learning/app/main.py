3ae4398c724e610f54844f1d3da3bdf1af502bf0
Bouajila <bouajilahamza@outlook.com>
2025-04-12 22:43:58 +0100

    Refactor strategy classes and simplify code structure for clarity

--- machine_learning/app/main.py
+++ machine_learning/app/main.py
@@ -5,50 +5,37 @@ import litserve as ls
 from abc import ABC, abstractmethod
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-
-# Add the project root directory to sys.path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 sys.path.append(project_root)
 from ingestion_pipeline import factory
 
-# Define the Strategy base class
 class UnsupervisedLearningStrategy(ABC):
     @abstractmethod
     def execute(self, data):
         pass
 
-# Concrete Strategy: K-Means
 class KMeansStrategy(UnsupervisedLearningStrategy):
     def execute(self, data):
-        # Placeholder for K-Means logic
-        # Example: KMeans clustering
         kmeans = KMeans(n_clusters=3)
         kmeans.fit(data)
         labels = kmeans.labels_
-        # Return the labels as the result
         return f"K-Means result for {labels.tolist()}"
 
-# Concrete Strategy: DBSCAN
 class DBSCANStrategy(UnsupervisedLearningStrategy):
     def execute(self, data):
-        # Placeholder for DBSCAN logic
-        # Example: DBSCAN clustering
         dbscan = DBSCAN(eps=0.5, min_samples=5)
         dbscan.fit(data)
         labels = dbscan.labels_
-        # Return the labels as the result
         return f"DBSCAN result for {labels.tolist()}"
 
-# Modify the SimpleLitAPI class to use the ingestion pipeline factory
+
 class SimpleLitAPI(ls.LitAPI):
     def setup(self, device):
         self.strategies = {
             "kmeans": KMeansStrategy(),
             "dbscan": DBSCANStrategy()
         }
-        self.selected_strategy = self.strategies["kmeans"]  # Default strategy
-
-        # Initialize the ingestion pipeline factory
+        self.selected_strategy = self.strategies["kmeans"] 
         self.ingestion_factory = factory
 
     def decode_request(self, request):
@@ -57,7 +44,6 @@ class SimpleLitAPI(ls.LitAPI):
     def predict(self, x):
         data, strategy_key, pipeline_key, file_path = x
         print(f"Data: {data}, Strategy: {strategy_key}, Pipeline: {pipeline_key}, File Path: {file_path}")
-        # Select the strategy
         try:
             file_path = os.path.join(project_root, file_path)
             data = pd.read_csv(file_path)         
